69f4a4c5411649d8c9845cad1cba3333f71e0e81
Maira Bello <maira.bello@inloco.com.br>
2019-06-12 11:12:46 -0300

    feature(label): Style Label

--- stories/Label/index.stories.js
+++ stories/Label/index.stories.js
@@ -1,31 +1,26 @@
 import React from 'react'
-import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { radios, text, withKnobs } from '@storybook/addon-knobs'
 
-import { Label, RemovableLabel } from '../../components'
+import { Label } from '../../components'
+
+const sizeKnob = () =>
+  radios('size', { Default: 'default', Small: 'small' }, 'default')
 
 storiesOf('Label', module)
-  .add('regular', () => (
-    <Label content="Regular label" />
+  .addDecorator(withKnobs)
+  .add('basic', () => (
+    <Label
+      content={text('Text', 'Label text')}
+      icon={text('icon')}
+      size={sizeKnob()}
+    />
   ))
   .add('removable', () => (
-    <React.Fragment>
-      <RemovableLabel content="Removable label (Small)" size="small" onRemove={action('removed')} />
-      <br /><br />
-      <RemovableLabel content="Removable label" onRemove={action('removed')} />
-      <br /><br />
-      <RemovableLabel content="Removable label (Large)" size="large" onRemove={action('removed')} />
-    </React.Fragment>
-  ))
-  .add('icon', () => (
-    <React.Fragment>
-      <Label content="Label with icon (Small)" icon="home" size="small" />
-      <br /><br />
-      <Label content="Label with icon" icon="home" />
-      <br /><br />
-      <Label content="Label with icon (Large)" icon="home" size="large" />
-    </React.Fragment>
-  ))
-  .add('pointing', () => (
-    <Label pointing="left">Pointing left</Label>
+    <Label
+      content={text('Text', 'Label text')}
+      size={sizeKnob()}
+      onRemove={action('onRemove')}
+    />
   ))
