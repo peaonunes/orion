1b1cf9e845deccecc9a0fa05bf7fb37b5b3f2914
Maira Bello <maira.bello@inloco.com.br>
2019-06-13 15:42:48 -0300

    feature(card): Style card

---
+++ src/Card/Card.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { text, withKnobs } from '@storybook/addon-knobs/react'
+
+import Card from './'
+
+storiesOf('Card', module)
+  .addDecorator(withKnobs)
+  .add('regular', () => (
+    <React.Fragment>
+      <Card header={text('Header', 'Header Title')} />
+    </React.Fragment>
+  ))
