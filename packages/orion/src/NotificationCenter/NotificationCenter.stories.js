596e0aebd828cc1caa14d9088722cdf24995c51c
Francisco Gileno de Oliveira Junior <gileno@gmail.com>
2019-06-26 16:20:23 -0300

    Merge pull request #78 from inloco/feature/notification-center
    Feature - Notification Center

---
+++ packages/orion/src/NotificationCenter/NotificationCenter.stories.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
+
+import Button from '../Button'
+import NotificationCenter from '.'
+
+const defaults = {
+  errorTitle: 'ErroR',
+  errorMessage: 'UnexpectedError',
+  successTitle: 'SuccesS'
+}
+const NotificationCenterDecorator = storyFn => (
+  <>
+    <NotificationCenter defaults={defaults} />
+    {storyFn()}
+  </>
+)
+
+storiesOf('NotificationCenter', module)
+  .addDecorator(withKnobs)
+  .addDecorator(NotificationCenterDecorator)
+  .add('Basic', () => (
+    <>
+      <div className="mb-16">
+        <Button
+          onClick={() => {
+            NotificationCenter.error("Everything is wrong, isn't it?")
+          }}>
+          Notify Error
+        </Button>
+      </div>
+      <div className="mb-16">
+        <Button
+          onClick={() => {
+            NotificationCenter.success("Everything is awesome, isn't it?")
+          }}>
+          Notify Success
+        </Button>
+      </div>
+      <div className="mb-16">
+        <Button
+          onClick={() => {
+            NotificationCenter.warning("DON'T TOUCH IT!!!")
+          }}>
+          Notify Warning
+        </Button>
+      </div>
+      <div className="mb-16">
+        <Button
+          onClick={() => {
+            NotificationCenter.inlineError("Everything is wrong, isn't it?")
+          }}>
+          Notify Inline Error
+        </Button>
+      </div>
+      <div className="mb-16">
+        <Button
+          onClick={() => {
+            NotificationCenter.inlineSuccess("Everything is awesome, isn't it?")
+          }}>
+          Notify Inline Success
+        </Button>
+      </div>
+      <div>
+        <Button
+          onClick={() => {
+            NotificationCenter.inlineWarning("DON'T TOUCH IT!!!")
+          }}>
+          Notify Inline Warning
+        </Button>
+      </div>
+    </>
+  ))
