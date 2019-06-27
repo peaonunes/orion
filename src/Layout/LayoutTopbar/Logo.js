906acced23c9f8da1c19ba17c3c5b67a55bf9328
Gileno Oliveira <gileno.oliveira@inlocomedia.com>
2019-06-27 14:06:49 -0300

    feature(Logo): Export Logo component

--- src/Layout/LayoutTopbar/Logo.js
+++ src/Layout/LayoutTopbar/Logo.js
@@ -8,8 +8,6 @@ const Logo = () => (
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink">
-    <title>logo-in-loco</title>
-    <desc>Created with Sketch.</desc>
     <g
       id="Page-1"
       stroke="none"
