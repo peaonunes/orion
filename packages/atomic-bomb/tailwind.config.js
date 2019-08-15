890f2bf1a3fe652dddc3a64174ff666ab2674568
Maira Bello <maira.bello@inloco.com.br>
2019-08-15 13:42:09 -0300

    feature(atomic-bomb): Add max-width 384px

--- packages/atomic-bomb/tailwind.config.js
+++ packages/atomic-bomb/tailwind.config.js
@@ -260,16 +260,7 @@ module.exports = {
       screen: '100vh'
     },
     maxWidth: {
-      xs: '20rem',
-      sm: '24rem',
-      md: '28rem',
-      lg: '32rem',
-      xl: '36rem',
-      '2xl': '42rem',
-      '3xl': '48rem',
-      '4xl': '56rem',
-      '5xl': '64rem',
-      '6xl': '72rem',
+      '384': '384px',
       full: '100%'
     },
     minHeight: theme => ({
