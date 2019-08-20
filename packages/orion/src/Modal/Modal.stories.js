dc998ea0f05ff326bfce0b33cd000e80acf43662
Marcel Rebouças <marcel.reboucas@inlocomedia.com>
2019-08-20 14:14:47 -0300

    feature(Modal): adds size support

--- packages/orion/src/Modal/Modal.stories.js
+++ packages/orion/src/Modal/Modal.stories.js
@@ -1,20 +1,31 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs } from '@storybook/addon-knobs/react'
+import { radios, withKnobs } from '@storybook/addon-knobs'
 
-import { Button, Modal, Icon, Header } from '../'
+import { Button, Modal } from '../'
 
 storiesOf('Modal', module)
   .addDecorator(withKnobs)
   .add('standard', () => {
+    const size = radios(
+      'Size',
+      {
+        Mini: 'mini',
+        Tiny: 'tiny',
+        Small: 'small',
+        Large: 'large',
+        'Full Screen': 'fullscreen'
+      },
+      'small'
+    )
     return (
-      <Modal trigger={<Button>Open Modal</Button>} closeIcon>
+      <Modal trigger={<Button>Open Modal</Button>} closeIcon size={size}>
         <Modal.Header icon="archive" content="Modal Title" />
         <Modal.Content>
-          <p>
-            Your inbox is getting full, would you like us to enable automatic
-            archiving of old messages?
-          </p>
+          <Modal.Description
+            content="Your inbox is getting full, would you like us to enable automatic
+            archiving of old messages?"
+          />
         </Modal.Content>
         <Modal.Actions>
           <Button>Cancel</Button>
