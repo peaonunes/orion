d154029d7a6400c029760f4e542e3e74fbef44ce
Marcel Rebouças <marcel.reboucas@inlocomedia.com>
2019-08-20 18:06:37 -0300

    feature(Modal.stories): adds the basic option

--- packages/orion/src/Modal/Modal.stories.js
+++ packages/orion/src/Modal/Modal.stories.js
@@ -4,22 +4,41 @@ import { radios, withKnobs } from '@storybook/addon-knobs'
 
 import { Button, Modal } from '../'
 
+const sizes = () =>
+  radios(
+    'Size',
+    {
+      Mini: 'mini',
+      Tiny: 'tiny',
+      Small: 'small',
+      Large: 'large',
+      'Full Screen': 'fullscreen'
+    },
+    'small'
+  )
+
 storiesOf('Modal', module)
   .addDecorator(withKnobs)
   .add('standard', () => {
-    const size = radios(
-      'Size',
-      {
-        Mini: 'mini',
-        Tiny: 'tiny',
-        Small: 'small',
-        Large: 'large',
-        'Full Screen': 'fullscreen'
-      },
-      'small'
-    )
     return (
-      <Modal trigger={<Button>Open Modal</Button>} closeIcon size={size}>
+      <Modal trigger={<Button>Open Modal</Button>} closeIcon size={sizes()}>
+        <Modal.Header icon="archive" content="Modal Title" />
+        <Modal.Content>
+          <Modal.Description
+            content="Your inbox is getting full, would you like us to enable automatic
+            archiving of old messages?"
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button>Cancel</Button>
+          <Button primary>Yes</Button>
+        </Modal.Actions>
+      </Modal>
+    )
+  })
+  .add('basic', () => {
+    return (
+      <Modal trigger={<Button>Open Modal</Button>} size={sizes()} basic>
         <Modal.Header icon="archive" content="Modal Title" />
         <Modal.Content>
           <Modal.Description
