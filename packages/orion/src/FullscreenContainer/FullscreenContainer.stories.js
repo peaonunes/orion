279c1a8cca85bcf4f3587ef951822299980e7e4b
Marcel Rebouças <marcel.reboucas@inlocomedia.com>
2019-08-23 17:40:25 -0300

    feature(FullscreenContainer): adds the Portal logic to the container

--- packages/orion/src/FullscreenContainer/FullscreenContainer.stories.js
+++ packages/orion/src/FullscreenContainer/FullscreenContainer.stories.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
 
-import { FullscreenContainer } from '../'
+import { FullscreenContainer, Button } from '../'
 
 storiesOf('FullscreenContainer', module)
   .addDecorator(withKnobs)
-  .add('Default', () => (
-    <FullscreenContainer title="My Title">
-      <div>My Content!</div>
-    </FullscreenContainer>
-  ))
+  .add('Default', () => {
+    return (
+      <FullscreenContainer
+        title="My Title"
+        trigger={<Button>Open Container</Button>}>
+        <div>My Content!</div>
+      </FullscreenContainer>
+    )
+  })
--- packages/orion/src/FullscreenContainer/index.js
+++ packages/orion/src/FullscreenContainer/index.js
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
+import ReactDOM from 'react-dom'
 
-import Icon from '../Icon'
+import { Icon } from '../'
 
-const FullscreenContainer = ({ children, title, className }) => {
+const FullscreenContainer = ({
+  title,
+  children,
+  className,
+  open,
+  onOpen,
+  defaultOpen,
+  onClose,
+  trigger
+}) => {
+  const [isOpen, setOpen] = useState(open || defaultOpen)
+
+  const openContainer = () => {
+    setOpen(true)
+    onOpen && onOpen()
+  }
+
+  const closeContainer = () => {
+    setOpen(false)
+    onClose && onClose()
+  }
+
   return (
-    <div className={cx('orion fullscreen-container', className)}>
-      <Icon name="close" />
-      <div className="fullscreen-container-title">{title}</div>
-      <div className="fullscreen-container-content">{children}</div>
-    </div>
+    <React.Fragment>
+      {<div onClick={openContainer}>{trigger}</div>}
+      {isOpen &&
+        ReactDOM.createPortal(
+          <div className={cx('orion fullscreen-container', className)}>
+            <Icon name="close" onClick={closeContainer} />
+            {title && <div className="fullscreen-container-title">{title}</div>}
+            <div className="fullscreen-container-content">{children}</div>
+          </div>,
+          document.body
+        )}
+    </React.Fragment>
   )
 }
 
 FullscreenContainer.propTypes = {
-  children: PropTypes.object,
   title: PropTypes.string,
-  className: PropTypes.string
+  children: PropTypes.node,
+  className: PropTypes.string,
+  open: PropTypes.bool,
+  onOpen: PropTypes.func,
+  defaultOpen: PropTypes.bool,
+  onClose: PropTypes.func,
+  trigger: PropTypes.node
 }
 
 export default FullscreenContainer
