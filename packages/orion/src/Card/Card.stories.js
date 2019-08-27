9089f9dc0b83c6f3b482ee85d0aaa29708535786
Marcel Rebouças <marcel.reboucas@inlocomedia.com>
2019-08-27 13:30:27 -0300

    feature(Card): adds the selectable property

--- packages/orion/src/Card/Card.stories.js
+++ packages/orion/src/Card/Card.stories.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { boolean, text, withKnobs } from '@storybook/addon-knobs/react'
+import cx from 'classnames'
 
-import { Button, Card } from '../'
+import { Button, Card, Icon } from '../'
 
 storiesOf('Card', module)
   .addDecorator(withKnobs)
@@ -25,6 +26,25 @@ storiesOf('Card', module)
               </Button>
             </React.Fragment>
           )}
+        </Card.Content>
+      </Card>
+    )
+  })
+  .add('selectable', () => {
+    const title = text('Title', 'Integrate SDK')
+    const selected = boolean(`Selected`, false)
+    return (
+      <Card
+        fluid={boolean('Fluid', false)}
+        selectable
+        selected={selected}
+        withCheckbox={boolean('With Checkbox', false)}>
+        <Card.Content className="flex flex-col items-center">
+          <Icon
+            name="code"
+            className={cx('text-gray-700', { 'text-wave-500': selected })}
+          />
+          <div className="mt-8">{title}</div>
         </Card.Content>
       </Card>
     )
--- packages/orion/src/Card/index.js
+++ packages/orion/src/Card/index.js
@@ -1,12 +1,40 @@
 import _ from 'lodash'
+import cx from 'classnames'
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Card as SemanticCard } from '@inloco/semantic-ui-react'
 
-const Card = ({ ...otherProps }) => {
-  return <SemanticCard {...otherProps} />
+import { Checkbox } from '../'
+
+const Card = ({
+  children,
+  className,
+  selectable,
+  selected,
+  withCheckbox,
+  ...otherProps
+}) => {
+  const classes = cx(
+    className,
+    { selectable: selectable },
+    { selected: selectable && selected }
+  )
+
+  return (
+    <SemanticCard className={classes} {...otherProps}>
+      {selectable && withCheckbox && <Checkbox checked={selected} readOnly />}
+      {children}
+    </SemanticCard>
+  )
 }
 
-Card.propTypes = {}
+Card.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  selectable: PropTypes.bool,
+  selected: PropTypes.bool,
+  withCheckbox: PropTypes.bool
+}
 
 Card.Content = SemanticCard.Content
 Card.Description = SemanticCard.Description
