650480ae9d5dd445850e167683dcc29f3b6a6ad5
Maira Bello <maira.bello@inloco.com.br>
2019-09-27 13:49:02 -0300

    feature(datepicker): Style disabled dates

--- packages/orion/src/Datepicker/Datepicker.stories.js
+++ packages/orion/src/Datepicker/Datepicker.stories.js
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
@@ -11,11 +12,18 @@ export const actions = {
 
 storiesOf('Datepicker', module)
   .addDecorator(withKnobs)
-  .add('basic', () => {
+  .add('Basic', () => {
     return (
       <Datepicker numberOfMonths={number('Number of months', 1)} {...actions} />
     )
   })
   .add('2 months', () => {
     return <Datepicker numberOfMonths={2} {...actions} />
+  })
+  .add('Disabled dates', () => {
+    const isDayBlocked = date => {
+      const dayOfWeek = date.format('dddd')
+      return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday'
+    }
+    return <Datepicker isDayBlocked={isDayBlocked} {...actions} />
   })
